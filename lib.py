ef901a9ce06c0c375f62398c017a1ee4e92ebbb8
hezhping <hezhangpin@sina.com>
2017-03-02 22:08:58 +0800

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 17:16:22 2017
+
+@author: zhang
+"""
+
+import numpy as np
+import theano.tensor as T
+import matplotlib.pyplot as plt
+import cv2
+
+def get_axis_aligned_BB(bbox):
+    width = bbox.shape[1]    
+    x_axis = np.arange(0,width,2,dtype='int32')
+    y_axis = np.arange(1,width,2,dtype='int32')
+    cx = np.mean(bbox[:,x_axis],axis=1)
+    cy = np.mean(bbox[:,y_axis],axis=1)
+    x1 = np.min(bbox[:,x_axis],axis=1)
+    x2 = np.max(bbox[:,x_axis],axis=1)
+    y1 = np.min(bbox[:,y_axis],axis=1)
+    y2 = np.max(bbox[:,y_axis],axis=1)
+    A1 = np.sqrt(np.sum((bbox[:,:2]-bbox[:,2:4])**2,axis=1))*np.sqrt(np.sum((bbox[:,2:4]-bbox[:,4:6])**2,axis=1))
+    A2 = (x2 - x1) * (y2 - y1)
+    s = np.sqrt(A1/A2)
+    w = s * (x2 - x1) + 1
+    h = s * (y2 - y1) + 1
+    bbox_rect = np.zeros((bbox.shape[0],4),dtype='float32')
+    bbox_rect[:,0] = cx
+    bbox_rect[:,1] = cy
+    bbox_rect[:,2] = w
+    bbox_rect[:,3] = h
+    return bbox_rect
+
+def crop(im, pos, model_size, original_size):
+    #print 'start crop'
+    [h,w,c] = im.shape
+    avg_chans = np.zeros(c)
+    for i in range(c):
+        avg_chans[i] = np.mean(im[:,:,i])
+    #print 'size: ',size
+    if not (type(original_size) is np.ndarray):
+        original_size = np.array([original_size,original_size],dtype='int32')
+    sz = original_size.astype('int32')
+    result = np.zeros((sz[0],sz[1],c),dtype=np.float32)
+    
+    # print 'result shape',result.shape
+    r = (sz+1)/2
+    y_min = int(round(pos[0] - r[0]))   
+    y_max = int(sz[0] + y_min) 
+    
+    
+    x_min = int(round(pos[1] - r[1]))
+    x_max = int(sz[1] + x_min)
+    
+    top_pad = max(0, 0 - y_min)    
+    down_pad = max(0, y_max - h) 
+    left_pad = max(0, 0 - x_min)
+    right_pad = max(0, x_max - w)
+    
+    y_min += top_pad
+    y_max += top_pad
+    x_min += left_pad
+    x_max += left_pad
+    #print 'x_min,x_max,y_min, y_max',x_min,x_max,y_min, y_max
+    #print 'top_pad,down_pad,left_pad, right_pad',top_pad,down_pad,left_pad, right_pad
+    #print type(top_pad),type(down_pad),type(left_pad),type(right_pad)
+    for i in range(c):
+        img = np.lib.pad(im[:,:,i],((top_pad,down_pad),(left_pad,right_pad)),'constant',constant_values=avg_chans[i])
+        #print 'img shape:',img.shape
+        result[:,:,i] = img[y_min:y_max,x_min:x_max]
+    #result = vggmean(result)
+    if (model_size != sz).any:
+        result = cv2.resize(result,dsize=(model_size,model_size),interpolation=cv2.INTER_CUBIC)    
+    return result   
+    
+def premnist():
+    data = np.load('/home/elliott/zzd_tool/iccv_track/mnist.npz')
+    x_train, y_train = data['x_train'], data['y_train']
+    del data
+    data = np.zeros((10,1000,784))
+    for _ in range(10):
+        data[_,:,:] = x_train[y_train==_,:][:1000]
+    del x_train, y_train
+    return np.reshape(data,(10,1000,28,28))
+    
+def mnist_data(data,shape,noise=None,heatmap=False,down=1):
+    n = shape[0]
+    f1 = np.zeros(shape,dtype='float32')
+    f2 = np.zeros_like(f1)
+    c, num = np.random.randint(0,10,n), np.random.randint(0,1000,(n,2))
+    pos = np.random.randint(40,61,(n,2))
+    move = np.random.randint(-14,15,(n,2))
+    for _ in range(shape[0]):
+        a, b = pos[_,0], pos[_,1]
+        da, db = a+move[_,0], b+move[_,1]
+        f1[_,0,a-14:a+14,b-14:b+14] = data[c[_],num[_,0]]
+        f2[_,0,da-14:da+14,db-14:db+14] = data[c[_],num[_,1]]
+        
+    if noise!=None:
+        f1 += noise*np.random.randn(shape[0],shape[1],shape[2],shape[3]).astype('float32')
+        f2 += noise*np.random.randn(shape[0],shape[1],shape[2],shape[3]).astype('float32')
+        
+    if heatmap:
+        heat = -np.ones((shape[0],shape[1],shape[2]/down,shape[3]/down),'float32')
+        Pos = pos+move
+        Pos = Pos/down
+        for _ in range(n):
+            heat[_,0,49+move[_,0]-4:49+move[_,0]+5,49+move[_,1]-4:49+move[_,1]+5] = 1
+        return f1, f2, move, heat
+    else:
+        return f1, f2, move
+        
+def fftprocess(x,y):
+    
+    X = np.fft.fftn(x,axes=(2,3),norm='ortho')
+    Y = np.fft.fftn(y,axes=(2,3),norm='ortho')
+    XX = np.array(np.real(np.fft.ifftn(np.conj(X)*X,axes=(2,3),norm='ortho')),dtype='float32')
+    XY = np.array(np.real(np.fft.ifftn(X*Y,axes=(2,3),norm='ortho')),dtype='float32')
+    XX = np.fft.ifftshift(XX,axes=(2,3))
+    XY = np.fft.ifftshift(XY,axes=(2,3))
+    return XX, XY
+    
+def fft_KCF(x,y,lam):
+    X = np.fft.fftn(x,axes=(2,3),norm='ortho')
+    Y = np.fft.fftn(y,axes=(2,3),norm='ortho')
+   
+    X_conj = np.conj(X)
+    '''
+    XX = np.array(np.real(np.fft.ifftn(X_conj*X,axes=(2,3),norm='ortho')),dtype='float32')
+    XY = np.array(np.real(np.fft.ifftn(X_conj*Y,axes=(2,3),norm='ortho')),dtype='float32')
+    XX = np.fft.ifftshift(XX,axes=(2,3))
+    XY = np.fft.ifftshift(XY,axes=(2,3))
+    '''
+    out = X_conj*Y/(X_conj*X+lam)
+    out = np.fft.ifftshift(np.fft.ifftn(out,norm='ortho',axes=(2,3)))
+    return np.real(out).astype('float32')
+    
+def show(x,y,n,save=False):
+    plt.figure(1)
+    for i in range(16):
+        plt.subplot(4,4,i+1)
+        plt.imshow(x[n+i,0])
+    plt.figure(2)
+    for i in range(16):
+        plt.subplot(4,4,i+1)
+        plt.imshow(y[n+i,0])
+    plt.show()
+    if save:
+        plt.savefig()
+    
+def bgr2gray(x,keepdim=True):
+    xg = 0.114*x[:,0,:,:]+0.587*x[:,1,:,:]+0.299*x[:,2,:,:]
+    if keepdim:
+        return xg[:,np.newaxis,:,:]
+    else:
+        return xg
+
+def score2move(x,per=0.85):
+    s_0, s_1 = x.shape[0]/2+np.mod(x.shape[0],2)-1, x.shape[1]/2+np.mod(x.shape[1],2)-1
+    #x = x-np.min(x)
+    #x = x/np.max(x)
+    if False:
+        v = np.argmax(x)
+        b = np.mod(v,x.shape[1])
+        a = (v-b)/x.shape[1]
+    
+    x[x<per] = 0
+    X, Y = np.array(range(x.shape[0])), np.array(range(x.shape[1])) 
+    X = np.reshape(X,(x.shape[0],1))
+    X = np.tile(X,(1,x.shape[1]))
+    Y = np.reshape(Y,(1,x.shape[1]))
+    Y = np.tile(Y,(x.shape[0],1))
+    sum_value = np.sum(x)
+    return np.sum(x*X)/sum_value-s_0, np.sum(x*Y)/sum_value-s_1
+    
+def Loss_log(predict,targets):
+    return T.sum(T.mean(T.log(1+T.exp(-predict*targets)),axis=(1,2,3)))
+    
+def Loss_svm(predict,targets,w,lam):
+    return T.mean(T.clip(1-predict*targets,0,1000),axis=(1,2,3))+lam*T.sum(w**2)
+    T.mean()
+def hanningwindow(n):
+    x = np.hanning(n)
+    x = np.array(x,dtype='float32')
+    x = x[:,np.newaxis]
+    x = np.dot(x,x.transpose())    
+    return x[np.newaxis,np.newaxis,:,:]
+    
+def fillscore(x,size,value=-1):
+    s = x.shape
+    y = value*np.ones((s[0]+2*size,s[1]+2*size),dtype=x.dtype)
+    y[size:s[0]+size,size:s[1]+size] = x
+    return y
+    
+def vggmean(im):
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68 
+    return im
+    
+if __name__=='__main__':
+    x = np.random.rand(100,100)
+    a,b = score2move(x)
+    
--- preprocessforVideo.py
+++ preprocessforVideo.py
@@ -13,22 +13,8 @@ import shutil
 import random
 import cv2
 import math
-
+from lib import get_axis_aligned_BB, crop
 np.random.seed(1000)
-def sample(img,center,size,num,type='Gaussian'):
-    if type == 'Gaussian':        
-        result = np.zeros((1,2),dtype = np.int16)
-        result[0,:] = center
-        result = result.repeat(num,0)   
-        #result += np.round(np.min(size)/100*np.minimum(1,np.maximum(-1,np.random.randn(num,2))))
-#        result[:,0] = result[:,0]+ np.round(size[0]/24*np.minimum(1,np.maximum(-1,np.random.randn(num))))
-#        result[:,1] = result[:,1]+ np.round(size[1]/24*np.minimum(1,np.maximum(-1,np.random.randn(num))))
-        result[:,0] += np.random.randint(-size[0]/4,size[0]/4,num)
-        result[:,1] += np.random.randint(-size[1]/4,size[1]/4,num)
-        result = np.maximum(0,result)
-#        result[:,0] = np.minimum(result[:,0],img.shape[1])
-#        result[:,1] = np.minimum(result[:,1],img.shape[0])
-        return result
         
 def get_Gaussian_label(sigma_f,label_size, obj_size, trans):
     if type(obj_size) is int:
@@ -50,99 +36,44 @@ def get_Gaussian_label(sigma_f,label_size, obj_size, trans):
     xx,xy = np.meshgrid(nx,ny)
     label = np.exp(-0.5*(xx**2 + xy**2)/sigma)
     label[label<0.3] = -1
-    return label   
+    return label  
     
-def crop(im, pos, model_size, original_size):
-    #print 'start crop'
-    [h,w,c] = im.shape
-    avg_chans = np.zeros(c)
-    for i in range(c):
-        avg_chans[i] = np.mean(im[:,:,i])
-    #print 'size: ',size
-    sz = int(original_size)
-    result = np.zeros((sz,sz,c),dtype=np.float32)
-    
-    # print 'result shape',result.shape
-    r = (sz+1)/2
-    x_min = int(round(pos[0] - r))   
-    x_max = int(sz + x_min) 
-    
-    
-    y_min = int(round(pos[1] - r))
-    y_max = int(sz + y_min)
-    
-    top_pad = max(0, 0 - x_min)    
-    down_pad = max(0, x_max - h) 
-    left_pad = max(0, 0 - y_min)
-    right_pad = max(0, y_max - w)
-    
-    x_min += top_pad
-    x_max += top_pad
-    y_min += left_pad
-    y_max += left_pad
-    #print 'x_min,x_max,y_min, y_max',x_min,x_max,y_min, y_max
-    #print 'top_pad,down_pad,left_pad, right_pad',top_pad,down_pad,left_pad, right_pad
-    #print type(top_pad),type(down_pad),type(left_pad),type(right_pad)
-    for i in range(c):
-        img = np.lib.pad(im[:,:,i],((top_pad,down_pad),(left_pad,right_pad)),'constant',constant_values=avg_chans[i])
-        #print 'img shape:',img.shape
-        result[:,:,i] = img[x_min:x_max,y_min:y_max]
-    if model_size != sz:
-        result = cv2.resize(result,dsize=(model_size,model_size),interpolation=cv2.INTER_CUBIC) 
-        result = cv2.GaussianBlur(result,(5,5),1.0)   
-    return result    
-    
-def sample_from_video(input_size =100, label_size =51):
-        
-    #input_size = 100 
-    num_sample = 8000 
+def sample_from_video(num_sample=8000,input_size =101, label_size =101,label_clip = 20):        
+
     # time_s = time.time()
     # num_sample_frame = 3
     pad_context = 0.5    
-    num_each_video = 400
-    spatial_sigma_f = 0.08   #Gaussian label sigma factor
+    num_each_video = 1000
+    #spatial_sigma_f = 0.08   #Gaussian label sigma factor
     
     #video_path = '/home/elliott/hezhping/OTB50' 
-    video_path = '/home/hp1/HeZhangPing/OTB50'
-    videoes = os.listdir(video_path)  
-    videoes.sort()
-    videoes =  videoes[:41] #取前30个视频用作训练集
-#    cur_path = os.getcwd()    
-#    #result_path = '/home/elliott/hezhping/video_train_set'
-#    data_path = cur_path + '/data'
-#    if os.path.exists(data_path):
-#        shutil.rmtree(data_path)
-#        
-#    os.mkdir(data_path)
-    #
-    #bbox_path = result_path + '/bbox.txt'
-    ##fid = open(bbox_path,'w')   
-    
-    
+    #video_path = '/home/hp1/HeZhangPing/OTB50'
+    video_path = '/home/hp1/HeZhangPing/vot2016'
+    videoes = os.listdir(video_path)    
     data = np.zeros((num_sample,3,input_size,input_size),dtype=np.float32)
     region = np.zeros((num_sample,3,input_size,input_size),dtype=np.float32)
     label = -np.ones((num_sample,1,label_size,label_size),dtype=np.float32) 
+    scales = np.zeros((num_sample,2),dtype= np.float32)
     translate = np.zeros((num_sample,2),dtype= np.float32)
     real_trans = np.zeros((num_sample,2),dtype= np.float32)
     index = 0
-    max_frame_ds = 3 #最大帧间差
+    max_frame_ds = 4 #最大帧间差
     
     #video = videoes[random.randint(0,len(videoes)-1)]
     sample_video = random.sample(videoes,15)
     #sample_video = videoes
-    #label_r = 8    #label的中心半径r
+    label_r = 5    #label的中心半径r
     for s_ in xrange(len(sample_video)):
         video = sample_video[s_]
-        cur_video_path = video_path + '/' + video     
+        cur_video_path = video_path + '/' + video       
+        gt_path = cur_video_path +'/groundtruth_rect.txt'
         
-        if video in ['Skating2', 'jump2']:
-            gt_path = (cur_video_path +'/groundtruth_rect.{}.txt').format(random.randint(1,2))
-        else:
-            gt_path = cur_video_path +'/groundtruth_rect.txt'
         gt = open(gt_path,'r')
         gt_arr = [line.strip() for line in gt.readlines()]
-        gt_arr = np.array([[int(col) for col in re.split(' |,|\t',row)]for row in gt_arr],dtype=np.int32)
-        #print gt_arr.dtype
+        gt_arr = np.array([[float(col) for col in re.split(' |\,|\t',row)]for row in gt_arr],dtype='float32')        
+
+        gt_arr = get_axis_aligned_BB(gt_arr)
+        gt_arr[:,:2] -= gt_arr[:,2:4]/2.0        
         center_arr = (np.round(gt_arr[:,:2] + gt_arr[:,2:4]/2-1)).astype(np.int32)
         
         #gt_file = pd.read_csv(gt_path,header=None, names = ['x','y','w','h'],sep = ',' )  
@@ -151,7 +82,7 @@ def sample_from_video(input_size =100, label_size =51):
         img_files = os.listdir(cur_video_path + '/img')
         img_files.sort()
         #im_arr = [np.array(Image.open(img_path + '/' + im),dtype=np.float32)for im in img_files]
-        im_arr = [np.array(cv2.imread(img_path + '/' + im),dtype=np.float32)for im in img_files]
+        im_arr = [np.array(cv2.imread(img_path + '/' + im),dtype='float32')for im in img_files]
         if im_arr[0].ndim ==2:
             im_arr = [im[...,np.newaxis].repeat(3,2) for im in im_arr]            
         
@@ -168,19 +99,15 @@ def sample_from_video(input_size =100, label_size =51):
         #for i in range(len(gt_arr)-1):
         for i in sample_list:
             #print 'i:',i       
-            pre_im = im_arr[i]
-           
+            pre_im = im_arr[i]       
             
             target_sz = np.array([gt_arr[i][3],gt_arr[i][2]], dtype=np.float32)
             hc_z = target_sz[0] + pad_context*np.sum(target_sz)
             wc_z = target_sz[1] + pad_context*np.sum(target_sz)
             sz = int(math.sqrt(hc_z*wc_z))    #实际采样的目标大小
-            max_trans = np.minimum(math.sqrt(target_sz.prod())*0.5,20) 
-                        
-            #pos = np.array([center_arr[i][1], center_arr[i][0]],dtype=np.float32) 
+            max_trans = np.minimum(math.sqrt(target_sz.prod())*0.5,20)                         
+           
             pos = center_arr[i][::-1]-1
-#            max_move = np.min(np.minimum(np.floor(target_sz*0.2),8))
-#            move = np.random.randint(-max_move,max_move,2)
             target = crop(pre_im,pos,input_size,sz) 
             target = np.transpose(target,(2,0,1))
             
@@ -200,11 +127,13 @@ def sample_from_video(input_size =100, label_size =51):
                     region[index,...] = search_region            
                     real_trans[index,...] = trans
                     #relative_move = input_size/2 + move*input_size/size
-                    relative_move = (cur_pos-pos)*(label_size)/float(sz)
+                    relative_move = trans*(label_size)/float(sz)
                     translate[index,...] = relative_move
                     
-                    #label[index,0,label_size/2-1+relative_move[0]-label_r:(input_size/down)/2-1+relative_move[0]+label_r,(input_size/down)/2-1+relative_move[1]-label_r:(input_size/down)/2-1+relative_move[1]+label_r] =1 
-                    label[index,0,...] = get_Gaussian_label(spatial_sigma_f,label_size,sz,relative_move)
+                    label_pos = np.round(label_size/2 -1 + relative_move).astype('int32')
+                    label[index,:,label_pos[0]-label_r:label_pos[0]+label_r,label_pos[1]-label_r:label_pos[1]+label_r]=1 
+                    scales[index,:] = gt_arr[cur_frame_id,2:4]/gt_arr[i,2:4] -1.0
+                    #label[index,0,...] = get_Gaussian_label(spatial_sigma_f,label_size,sz,relative_move)
         #            redius = 0.3*np.array([gt_arr[i+1][3],gt_arr[i+1][2]],dtype=np.float32)*(input_size/down)/size
         #            range_move_left = (relative_move - redius)
         #            range_move_right = (relative_move + redius)
@@ -218,9 +147,9 @@ def sample_from_video(input_size =100, label_size =51):
                     if index==num_sample:
                         data = 2*data/255.0-1
                         region = 2*region/255.0-1
-                        label = label[:,:,20:81,20:81]
+                        label = label[:,:,label_clip:(label_size-label_clip),label_clip:(label_size-label_clip)]
                         #print num_wrong_sample
-                        return data,region,label,translate,real_trans
+                        return data,region,label,scales,translate,real_trans
     
         #print index-1
 #        data_1 = data[index-1,...]
@@ -242,24 +171,22 @@ def sample_from_video(input_size =100, label_size =51):
 #        time_e = time.time()
 #        print 'time cost:{}s'.format(time_e-time_s)
     data = data[:index,...]
-    region = region[:index,...]
-    data = 2*data/255.0-1
-    region = 2*region/255.0-1
-    label = label[:index]  
-    label = label[:,:,20:81,20:81]
+    region = region[:index,...]  
+    label = label[:index] 
+    scales = scales[:index]
     translate = translate[:index]
     real_trans = real_trans[:index]
+    
+    data = 2*data/255.0-1
+    region = 2*region/255.0-1
+    label = label[:,:,label_clip:(label_size-label_clip),label_clip:(label_size-label_clip)]
     #print num_wrong_sample
     #data = data/255-0.5
     #region = region/255-0.5
-    return data,region,label,translate,real_trans
+    return data,region,label,scales,translate,real_trans
     
 if __name__ == '__main__':
-    #label_size = np.array([5,5])
-#    label_size = 5
-#    trans = np.array([2,2])
-#    obj_size = np.array([3,3])
-#    label = get_Gaussian_label(0.1,label_size, obj_size, trans)
-    [data,region,label,trans,real_trans]= sample_from_video()
+
+    [data,region,label,scales,trans,real_trans]= sample_from_video()
     #np.savez('sampleFromVideo.npz',data=data,region=region,label=label,trans=trans)
     
